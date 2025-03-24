5531ec6657a5c237dd954f5a699fc6b49553742e
Gururaj Pandurangi <grpandurangi@gmail.com>
2025-03-24 09:26:55 +0530

    lb redirect to https

--- infra/deploy/load_balancer.tf
+++ infra/deploy/load_balancer.tf
@@ -52,6 +52,23 @@ resource "aws_lb_listener" "api" {
   load_balancer_arn = aws_lb.api.arn
   port              = 80
   protocol          = "HTTP"
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "api_https" {
+  load_balancer_arn = aws_lb.api.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  certificate_arn = "arn:aws:acm:us-east-1:444014219622:certificate/c627530c-74e8-4f3f-b771-c8641763246e"
 
   default_action {
     type             = "forward"
