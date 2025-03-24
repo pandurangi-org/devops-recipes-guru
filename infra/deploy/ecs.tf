159995bbfb84c4c8ff238a5a7dcd5972acb8457f
Gururaj Pandurangi <grpandurangi@gmail.com>
2025-03-24 08:09:53 +0530

    added efs for media

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -89,6 +89,11 @@ resource "aws_ecs_task_definition" "api" {
           readOnly      = false
           containerPath = "/vol/web/static"
           sourceVolume  = "static"
+          },
+          {
+            readOnly      = false
+            containerPath = "/vol/media"
+            sourceVolume  = "efs-media"
         }
       ],
       logConfiguration = {
@@ -123,6 +128,11 @@ resource "aws_ecs_task_definition" "api" {
           readOnly      = true
           containerPath = "/vol/static"
           sourceVolume  = "static"
+          },
+          {
+            readOnly      = true
+            containerPath = "/vol/web/media"
+            sourceVolume  = "efs-media"
         }
       ]
       logConfiguration = {
@@ -140,6 +150,19 @@ resource "aws_ecs_task_definition" "api" {
     name = "static"
   }
 
+  volume {
+    name = "efs-media"
+    efs_volume_configuration {
+      file_system_id     = aws_efs_file_system.media.id
+      transit_encryption = "ENABLED"
+
+      authorization_config {
+        access_point_id = aws_efs_access_point.media.id
+        iam             = "DISABLED"
+      }
+    }
+  }
+  
   runtime_platform {
     operating_system_family = "LINUX"
     cpu_architecture        = "X86_64"
@@ -169,6 +192,19 @@ resource "aws_security_group" "ecs_service" {
       aws_subnet.private_b.cidr_block,
     ]
   }
+
+
+ # NFS Port for EFS volumes
+  egress {
+    from_port = 2049
+    to_port   = 2049
+    protocol  = "tcp"
+    cidr_blocks = [
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_b.cidr_block,
+    ]
+  }
+
 
   # HTTP inbound access
   ingress {
